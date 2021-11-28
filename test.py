7baf671bd8ec8ec102765d22976305e07e1a0a21
Gabriel Klug <klug123@gmail.com>
2021-11-28 17:19:35 -0500

    Falhando a pipeline

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline DevOps - Gabriel Klug.")
+        self.assertRegex(result.data.decode(), "Pipeline não DevOps - Gabriel Klug.")
 
 
 if __name__ == "__main__":
